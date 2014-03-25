d3206c6220c7611a034686734ac9645daab349b6
Alain <a.fernandez.arrieta@accenture.com>
2014-03-25 15:35:21 +0100

    Metido error en método suma para ver cómo casca

--- pru1/src/main/java/acn/b/gmelabs/test/Prueba.java
+++ pru1/src/main/java/acn/b/gmelabs/test/Prueba.java
@@ -2,7 +2,7 @@ package acn.b.gmelabs.test;
 
 public class Prueba {
 	public int suma(int num1, int num2) {
-		return num1 + num2;
+		return num1 + num2 + 1;
 	}
 	public int resta(int num1, int num2) {
 		return num1 - num2;
