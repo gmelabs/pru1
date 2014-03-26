f9359890675fdbe772aa3a45ea9739cf908778ed
Alain <a.fernandez.arrieta@accenture.com>
2014-03-26 09:24:47 +0100

    Corregido el error en suma

--- pru1/src/main/java/acn/b/gmelabs/test/Prueba.java
+++ pru1/src/main/java/acn/b/gmelabs/test/Prueba.java
@@ -2,7 +2,7 @@ package acn.b.gmelabs.test;
 
 public class Prueba {
 	public int suma(int num1, int num2) {
-		return num1 + num2 + 1;
+		return num1 + num2;
 	}
 	public int resta(int num1, int num2) {
 		return num1 - num2;
